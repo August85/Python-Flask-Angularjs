f94037c7aa266b089da8ff380398d4b0a4abc2b9
Abhimanyu Katyayan <abhimanyu.k@ugamsolutions.com>
2015-04-04 00:37:16 +0530

    modifying README and removing unused imports.

--- server/app/server.py
+++ server/app/server.py
@@ -1,12 +1,6 @@
 import os
-import sys
 from flask import Flask
 from flask.ext import restful
-from flask.ext.restful import reqparse, Api
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bcrypt import Bcrypt
-from flask.ext.httpauth import HTTPBasicAuth
-from os.path import expanduser
 
 basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../')
 ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../static/app')
@@ -16,12 +10,6 @@ app.config.from_object('app.config')
 
 # flask-restful
 api = restful.Api(app)
-
-# flask-bcrypt
-flask_bcrypt = Bcrypt(app)
-
-# flask-httpauth
-auth = HTTPBasicAuth()
 
 @app.after_request
 def after_request(response):
